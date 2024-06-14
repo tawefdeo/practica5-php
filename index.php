5fdc69be91e14935ad2991b0a5774f48c6068c5a
Tadeo Contreras <147439035+tawefdeo@users.noreply.github.com>
2024-06-14 04:06:52 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>codigo en php</title>
+  </head>
+  <body>
+    <?php 
+    $txt=" grupo 4A"; 
+    echo "Hola" . $txt . "!"; 
+                        ?>
+  </body>
+</html>
